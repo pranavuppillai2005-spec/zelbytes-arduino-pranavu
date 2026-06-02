d6686c494533af0006f55200ef1d300385ca88a9
pranavuppillai2005-spec <pranavuppillai2005@gmail.com>
2026-06-02 14:44:27 +0530

    relay_valve.ino

--- TASK 2/relay_valve/relay_valve.ino
+++ TASK 2/relay_valve/relay_valve.ino
@@ -1,18 +1,8 @@
-// Relay Controlled Solenoid Valve
-// Relay IN -> Arduino D7
-// Relay VCC -> 5V
-// Relay GND -> GND
-//
-// IMPORTANT:
-// Solenoid valve must be powered using an external supply.
-// Never connect the valve directly to an Arduino GPIO pin.
-
 const int relayPin = 7;
 
 void setup() {
   pinMode(relayPin, OUTPUT);
 
-  // Relay OFF initially
   digitalWrite(relayPin, LOW);
 
   Serial.begin(9600);
@@ -21,12 +11,10 @@ void setup() {
 
 void loop() {
 
-  // Turn valve ON
   Serial.println("Valve ON");
   digitalWrite(relayPin, HIGH);
   delay(5000);
 
-  // Turn valve OFF
   Serial.println("Valve OFF");
   digitalWrite(relayPin, LOW);
   delay(5000);
