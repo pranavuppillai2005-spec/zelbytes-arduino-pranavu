c616ee70a20fccc0cb24f849e00b788568c83978
pranavuppillai2005-spec <pranavuppillai2005@gmail.com>
2026-06-02 11:07:16 +0530

    Blink.ino

--- src/Blink/Blink.ino
+++ src/Blink/Blink.ino
@@ -1,1 +1,10 @@
-// Blink sketch
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+}
