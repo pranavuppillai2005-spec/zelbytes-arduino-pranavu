7825ff6644e29d0b8e021dc98139da1c837a1374
Pranavu P Pillai <pranavuppillai2005@gmail.com>
2026-06-04 12:30:33 +0530

    manual_irrigation.ino

--- TASK3/src/manual_irrigation.ino
+++ TASK3/src/manual_irrigation.ino
@@ -1,30 +1,36 @@
 #define BUTTON_PIN 2
 #define RELAY_PIN 8
-#define IRRIGATION_TIME 5000
+#define LED_PIN 13
 
-bool relayActive = false;
-unsigned long startTime = 0;
+#define IRRIGATION_TIME 5000   // Valve ON time in milliseconds (5 sec)
+
+bool valveActive = false;
+unsigned long valveStartTime = 0;
+
 unsigned long lastDebounceTime = 0;
 const unsigned long debounceDelay = 50;
 
-int lastButtonState = HIGH;
 int buttonState = HIGH;
+int lastButtonReading = HIGH;
 
 void setup() {
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   pinMode(RELAY_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
 
   digitalWrite(RELAY_PIN, LOW);
+  digitalWrite(LED_PIN, LOW);
 
   Serial.begin(9600);
-
-  Serial.println("Manual Irrigation Controller");
+  Serial.println("Manual Irrigation Controller Started");
 }
 
 void loop() {
+
   int reading = digitalRead(BUTTON_PIN);
 
-  if (reading != lastButtonState) {
+  // Debounce
+  if (reading != lastButtonReading) {
     lastDebounceTime = millis();
   }
 
@@ -33,33 +39,39 @@ void loop() {
     if (reading != buttonState) {
       buttonState = reading;
 
-      if (buttonState == LOW && !relayActive) {
+      // Button Pressed
+      if (buttonState == LOW && !valveActive) {
 
-        relayActive = true;
-        startTime = millis();
+        valveActive = true;
+        valveStartTime = millis();
 
         digitalWrite(RELAY_PIN, HIGH);
+        digitalWrite(LED_PIN, HIGH);
 
+        Serial.println("-------------------");
         Serial.println("Valve OPENED");
         Serial.print("Timestamp: ");
-        Serial.print(startTime);
+        Serial.print(valveStartTime);
         Serial.println(" ms");
       }
     }
   }
 
-  lastButtonState = reading;
+  lastButtonReading = reading;
 
-  if (relayActive &&
-      (millis() - startTime >= IRRIGATION_TIME)) {
+  // Auto Close Valve
+  if (valveActive &&
+      (millis() - valveStartTime >= IRRIGATION_TIME)) {
 
-    relayActive = false;
+    valveActive = false;
 
     digitalWrite(RELAY_PIN, LOW);
+    digitalWrite(LED_PIN, LOW);
 
     Serial.println("Valve CLOSED");
     Serial.print("Timestamp: ");
     Serial.print(millis());
     Serial.println(" ms");
+    Serial.println("-------------------");
   }
 }
