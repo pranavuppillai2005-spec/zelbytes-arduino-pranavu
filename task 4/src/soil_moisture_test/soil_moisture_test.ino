a78a600e57d0cc74a6beb858b9d74434a869dccf
Pranavu P Pillai <pranavuppillai2005@gmail.com>
2026-06-09 10:45:56 +0530

    soil_moisture_test.ino

--- task 4/src/soil_moisture_test/soil_moisture_test.ino
+++ task 4/src/soil_moisture_test/soil_moisture_test.ino
@@ -1,1 +1,15 @@
+#define SOIL_PIN A1
 
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  int soil = analogRead(SOIL_PIN);
+
+  Serial.print("Soil Raw: ");
+  Serial.println(soil);
+
+  delay(1000);
+}
