cfd64e9d0babe3924da5ca0aa44e759b2c39231e
Pranavu P Pillai <pranavuppillai2005@gmail.com>
2026-06-09 10:44:56 +0530

    dht22_test.ino

--- task 4/src/dht22_test/dht22_test.ino
+++ task 4/src/dht22_test/dht22_test.ino
@@ -1,1 +1,24 @@
+#include <DHT.h>
 
+#define DHTPIN 2
+#define DHTTYPE DHT22
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop() {
+
+  Serial.print("Temperature: ");
+  Serial.print(dht.readTemperature());
+
+  Serial.print(" C  Humidity: ");
+  Serial.print(dht.readHumidity());
+
+  Serial.println(" %");
+
+  delay(2000);
+}
